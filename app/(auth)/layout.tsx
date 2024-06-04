51b44506abc8baa5c0ca8ac71a9cf3f7d36837bc
Umer Nasir <umer.nasir@devopsplex.com>
2024-06-04 12:34:55 +0500

    feat: add auth pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,43 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
+import { ClerkLoading, ClerkLoaded, SignIn } from "@clerk/nextjs";
+import type { PropsWithChildren } from "react";
+
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
+  return (
+    <>
+      <ClerkLoading>
+        <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[400px] w-[400px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[400px]" />
+                <Skeleton className="h-4 w-[400px]" />
+              </div>
+            </div>
+          </div>
+          <div className="hidden bg-blue-600 text-white lg:block">
+            <div className="h-full flex flex-col justify-center items-center gap-y-4">
+              <Image src="/logo.svg" alt="Logo" width={150} height={150} />
+              <p className="text-sm">One place to manage it all.</p>
+            </div>
+          </div>
+        </section>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <section className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col justify-center items-center">
+            {children}
+          </div>
+          <div className="hidden bg-blue-600 text-white lg:block">
+            <div className="h-full flex flex-col justify-center items-center gap-y-4">
+              <Image src="/logo.svg" alt="Logo" width={150} height={150} />
+              <p className="text-sm">One place to manage it all.</p>
+            </div>
+          </div>
+        </section>
+      </ClerkLoaded>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
@@ -12,10 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <main>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
